a7145ba089804d22c3ffd04214427e1126f78c79
Gennady Vedernikov <revoltlight@icloud.com>
2023-03-06 20:20:28 +0300

    Finish project

---
+++ FathersKickMarathonV2/MyModalViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MyModalViewController.swift
+//  FathersKickMarathonV2
+//
+//  Created by Геннадий Ведерников on 06.03.2023.
+//
+
+import UIKit
+
+class MyModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- FathersKickMarathonV2/ViewController.swift
+++ FathersKickMarathonV2/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var animator: UIViewPropertyAnimator?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let tapGestureRecognizer1 = UITapGestureRecognizer(target: self, action: #selector(buttonTapped(_:)))
+        button1.addGestureRecognizer(tapGestureRecognizer1)
+
+        let tapGestureRecognizer2 = UITapGestureRecognizer(target: self, action: #selector(buttonTapped(_:)))
+        button2.addGestureRecognizer(tapGestureRecognizer2)
+
+        let tapGestureRecognizer3 = UITapGestureRecognizer(target: self, action: #selector(buttonTapped(_:)))
+        button3.addGestureRecognizer(tapGestureRecognizer3)
+        
     }
+    
+    @objc func buttonTapped(_ sender: UITapGestureRecognizer) {
+        if sender.state == .ended {
+            
+            let duration = 0.5
+            let timingParameters = UICubicTimingParameters(animationCurve: .easeInOut)
+            animator = UIViewPropertyAnimator(duration: duration, timingParameters: timingParameters)
 
+            animator?.addAnimations {
+                sender.view?.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+            }
 
+            animator?.addCompletion { _ in
+                sender.view?.transform = CGAffineTransform.identity
+            }
+
+            animator?.startAnimation()
+        } else if sender.state == .cancelled || sender.state == .failed || sender.state == .recognized {
+            animator?.isReversed = true
+        }
+    }
 }
 
